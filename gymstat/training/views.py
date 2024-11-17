67d465a136150aafd1cbff36e9a0d3caf07652d9
Flynotfly <Alteria2004@gmail.com>
2024-11-17 23:44:27 +0400

    Create TrainingCreateUpdateView

--- gymstat/training/views.py
+++ gymstat/training/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.generic.edit import CreateView
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from .models import Training
+from .forms import TrainingForm
+
+
+class TrainingCreateUpdateView(CreateView):
+    model = Training
+    form_class = TrainingForm
+    template_name = 'training/training.html'
+
+    def get_object(self, **kwargs):
+        pk = self.kwargs.get('pk')
+        if pk:
+            return get_object_or_404(Training, pk=pk)
+        else:
+            return None
+
+    def form_valid(self, form):
+        training = form.save()
+        return JsonResponse({
+            'success': True,
+            'id': training.id,
+        })
+
+    def form_invalid(self, form):
+        return JsonResponse({
+            'success': False,
+            'errors': form.errors,
+        })
