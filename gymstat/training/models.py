4ee355a7c2f5c688cafab347efaea2eb7ac59e87
Flynotfly <Alteria2004@gmail.com>
2024-11-17 22:34:12 +0400

    Create Training model

--- gymstat/training/models.py
+++ gymstat/training/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Training(models.Model):
+    owner = models.ForeignKey(
+        User,
+        related_name='trainings',
+        on_delete=models.CASCADE,
+    )
+    time = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True)
