e88a973cfaad82152dd38bdb380b5cfbf9dd81e0
Flynotfly <Alteria2004@gmail.com>
2024-11-17 23:16:42 +0400

    Add Training and Exercise models to admin panel

--- gymstat/training/admin.py
+++ gymstat/training/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Training, Exercise
+
+
+class TrainingInline(admin.StackedInline):
+    model = Exercise
+    fields = ['order', 'weight', 'repetitions']
+
+
+@admin.register(Training)
+class TrainingAdmin(admin.ModelAdmin):
+    list_display = ['owner', 'time', 'description']
+    filter = ['owner']
+    inlines = [TrainingInline]
+
