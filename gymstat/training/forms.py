30a7850f615b16baf226f265f4cbefc8d6119c9b
Flynotfly <Alteria2004@gmail.com>
2024-11-18 23:02:49 +0400

    Delete __init__ method from training form

--- gymstat/training/forms.py
+++ gymstat/training/forms.py
@@ -8,8 +8,3 @@ class TrainingForm(forms.ModelForm):
         model = Training
         fields = ['conducted', 'description']
 
-    def __init__(self, *args, **kwargs):
-        user = kwargs.pop('user', None)
-        super().__init__(*args, **kwargs)
-        if user:
-            self.instance.owner = user
