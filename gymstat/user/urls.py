ed4435a6145c35b3d6c5f0af49738d6a675ac50f
Flynotfly <Alteria2004@gmail.com>
2024-11-21 19:17:10 +0400

    Add trainings list

--- gymstat/user/urls.py
+++ gymstat/user/urls.py
@@ -8,5 +8,6 @@ app_name = 'user'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('trainings/', views.training_list, name='trainings'),
     path('', views.dashboard, name='dashboard'),
 ]
--- gymstat/user/views.py
+++ gymstat/user/views.py
@@ -1,6 +1,19 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from training.models import Training
+
+
+def training_list(request):
+    trainings = Training.objects.filter(owner=request.user)
+    return render(
+        request,
+        'user/trainings.html',
+        {
+            'trainings': trainings,
+        }
+    )
+
 
 @login_required
 def dashboard(request):
