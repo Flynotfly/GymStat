d4913926e6facfecb362e5124595f2d72ad7831f
Flynotfly <Alteria2004@gmail.com>
2025-01-19 20:32:59 +0400

    add exercise formset template

--- gymstat/training/forms.py
+++ gymstat/training/forms.py
@@ -64,6 +64,8 @@ ExerciseFormSet = forms.inlineformset_factory(
     extra=0,
 )
 
+ExerciseFormSet.template_name_div = 'training/forms/exercise_formset.html'
+
 
 class ExerciseTypeForm(forms.ModelForm):
     class Meta:
