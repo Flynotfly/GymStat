814f6901fd4a122d43d0462537d22f864541ab68
Flynotfly <Alteria2004@gmail.com>
2025-02-07 19:19:13 +0400

    move jwt auth

--- gymstat/gymstat/urls.py
+++ gymstat/gymstat/urls.py
@@ -19,15 +19,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
     path('user/', include('user.urls', namespace='user')),
     path('training/', include('training.urls', namespace='training')),
--- gymstat/user/urls.py
+++ gymstat/user/urls.py
@@ -1,11 +1,14 @@
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, include
 
+from .api import urls as api_urls
 from . import views
 
 app_name = 'user'
 
 urlpatterns = [
+    path('api/', include(api_urls, namespace='api')),
+
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('trainings/', views.training_list, name='trainings'),
