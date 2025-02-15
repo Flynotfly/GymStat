29ec302b879b08a89b935d2862ba3893ebdf8230
Flynotfly <Alteria2004@gmail.com>
2025-02-15 15:17:41 +0400

    add selfsigned cert and key to git and builted react

---
+++ frontend/dist/assets/Body-D8K0Cvo8.js
@@ -0,0 +1,1 @@
+import{j as o,T as r}from"./index-Bq7oUHB8.js";function n(){return o.jsx(r,{component:"h2",variant:"h6",sx:{mb:2},children:"Body"})}export{n as default};
---
+++ frontend/dist/assets/Tasks-BAfbWtVc.js
@@ -0,0 +1,1 @@
+import{j as s,T as a}from"./index-Bq7oUHB8.js";function t(){return s.jsx(a,{component:"h2",variant:"h6",sx:{mb:2},children:"Tasks"})}export{t as default};
