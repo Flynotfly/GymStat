f3c06483c3a68e0eef2f818ace127bfa98d29a78
Flynotfly <Alteria2004@gmail.com>
2025-02-18 09:59:52 +0400

    Add promo tsx

---
+++ frontend/vite-react/src/pages/Promo.tsx
@@ -0,0 +1,9 @@
+import Typography from "@mui/material/Typography";
+
+export default function Promo() {
+  return (
+    <Typography component="h2" variant="h6" sx={{ mb: 2 }}>
+      This is promo
+    </Typography>
+  );
+}
