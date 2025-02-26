3ba2590a7d3df0ac9085470616337d4b06ffa43a
Flynotfly <Alteria2004@gmail.com>
2025-02-26 17:59:43 +0400

    Add AuthChangeRedirector

--- frontend/vite-react/src/main.tsx
+++ frontend/vite-react/src/main.tsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {StyledEngineProvider} from "@mui/material";
 import App from "./App.tsx";
-import UserProvider from "./providers/user/UserProvider.tsx";
+import {AuthChangeRedirector} from "./auth";
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <StyledEngineProvider>
-      <UserProvider>
+      <AuthChangeRedirector>
         <App />
-      </UserProvider>
+      </AuthChangeRedirector>
     </StyledEngineProvider>
   </StrictMode>,
 )
