e1235e92f58f7b05bf0966b671819f53340e4164
Flynotfly <Alteria2004@gmail.com>
2025-02-26 21:33:14 +0400

    Add AuthContent provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {StyledEngineProvider} from "@mui/material";
 import App from "./App.tsx";
+import {AuthContextProvider} from "./auth";
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <StyledEngineProvider>
+      <AuthContextProvider>
         <App />
+      </AuthContextProvider>
     </StyledEngineProvider>
   </StrictMode>,
 )
