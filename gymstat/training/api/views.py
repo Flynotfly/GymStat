4f7b6888b56c3c1dfb8e88b28fb61ea5c8c958a5
Flynotfly <Alteria2004@gmail.com>
2025-03-14 23:49:28 +0400

    [Enhancement] Create GetTraining view

--- gymstat/training/api/views.py
+++ gymstat/training/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import status
-from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -69,6 +69,12 @@ class AllTrainingsList(ListAPIView):
 
     def get_queryset(self):
         return Training.objects.filter(owner=self.request.user)
+
+
+class GetTraining(RetrieveAPIView):
+    serializer_class = TrainingOverallSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = Training.objects.all()
 
 
 # class GetAllTrainingsAPIView(APIView):
