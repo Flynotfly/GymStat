a1c89c7d0921cadea2d6fdd39d0b6ca64af56dc1
Flynotfly <Alteria2004@gmail.com>
2025-03-14 17:44:35 +0400

    [Refactor] Update training model: add score field, remove duration

--- gymstat/training/models.py
+++ gymstat/training/models.py
@@ -10,9 +10,9 @@ class Training(models.Model):
         on_delete=models.CASCADE,
     )
     conducted = models.DateTimeField()
-    duration = models.DurationField(blank=True, null=True)
     title = models.CharField(max_length=70, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
+    score = models.PositiveIntegerField(default=10)
 
     class Meta:
         indexes = [models.Index(fields=["-conducted"])]
