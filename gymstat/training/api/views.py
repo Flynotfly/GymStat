5c0ba5cba5e2933fe51e297464c233e0f523c1b0
Flynotfly <Alteria2004@gmail.com>
2025-03-23 22:44:52 +0400

    [Enhancement] Remove unused views and serializerrs

--- gymstat/training/api/views.py
+++ gymstat/training/api/views.py
@@ -1,9 +1,5 @@
-from rest_framework import status
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.views import APIView
-
 from django.db.models import Q
 
 from ..models import ExerciseType, Training
@@ -48,21 +44,6 @@ class ExerciseTypeCreateView(CreateAPIView):
         serializer.save(owner=self.request.user)
 
 
-# class LastTrainingAPIView(APIView):
-#     permission_classes = [IsAuthenticated]
-#
-#     def get(self, request):
-#         last_training = Training.objects.filter(owner=request.user).first()
-#         if not last_training:
-#             return Response(
-#                 {"error": "No previous training found"},
-#                 status=status.HTTP_404_NOT_FOUND,
-#             )
-#
-#         serializer = TrainingSerializer(last_training)
-#         return Response(serializer.data)
-
-
 class TrainingOverallListView(ListAPIView):
     serializer_class = TrainingOverallSerializer
     permission_classes = [IsAuthenticated]
@@ -94,51 +75,3 @@ class TrainingCreateView(CreateAPIView):
 
     def get_queryset(self):
         return Training.objects.filter(owner=self.request.user)
-
-# class GetAllTrainingsAPIView(APIView):
-#     permission_classes = [IsAuthenticated]
-#
-#     def get(self, request):
-#         trainings = Training.objects.filter(owner=request.user)
-#         if not trainings.exists():
-#             return Response(
-#                 {'error': 'No trainings found'},
-#                 status=status.HTTP_404_NOT_FOUND
-#             )
-#
-#         serializer = TrainingSerializer(trainings, many=True)
-#         return Response(serializer.data)
-
-#
-# class GetAllTrainingsAPIView(APIView):
-#     permission_classes = [IsAuthenticated]
-#
-#     def get(self, request):
-#         exercise_type_id = request.GET.get("exercise_type")
-#         if not exercise_type_id:
-#             return Response(
-#                 {"error": "No exercise type id provided"},
-#                 status=status.HTTP_400_BAD_REQUEST,
-#             )
-#         try:
-#             exercise_type_id = int(exercise_type_id)
-#         except ValueError:
-#             return Response(
-#                 {"error": "Invalid exercise type id"},
-#                 status=status.HTTP_400_BAD_REQUEST,
-#             )
-#
-#         trainings = Training.objects.filter(owner=request.user).all()
-#         if not trainings.exists():
-#             return Response(
-#                 {"error": "No trainings found"},
-#                 status=status.HTTP_404_NOT_FOUND,
-#             )
-#
-#         serializer = TrainingSummarySerializer(
-#             trainings,
-#             many=True,
-#             context={"exercise_type_id": exercise_type_id},
-#         )
-#
-#         return Response(serializer.data, status=status.HTTP_200_OK)
