d0fc5c950b82b8864b66ef99fe678d36af068f07
Flynotfly <Alteria2004@gmail.com>
2025-03-24 22:48:27 +0400

    Create AuthData interface

---
+++ frontend/src/types/auth.d.ts
@@ -0,0 +1,19 @@
+export interface AuthData {
+  user: User;
+  methods: AuthMethod[];
+}
+
+export interface User {
+  id: number;
+  display: string;
+  has_usable_password: boolean;
+  email: string;
+  username: string;
+}
+
+export interface AuthMethod {
+  method: string;
+  at: number; // Unix timestamp
+  email: string;
+  username: string;
+}
