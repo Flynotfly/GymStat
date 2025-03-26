bb979b278bd77eda42d000fac93ac2e8b3611f08
Flynotfly <Alteria2004@gmail.com>
2025-03-26 14:26:35 +0400

    Create RecordSerializer

--- gymstat/body_metrics/serializers.py
+++ gymstat/body_metrics/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Metric
+from .models import Metric, Record
 
 
 class MetricSerializer(serializers.ModelSerializer):
@@ -11,3 +11,11 @@ class MetricSerializer(serializers.ModelSerializer):
         fields = '__all__'
         read_only_fields = ('owner', 'admin', 'created_at', 'edited_at')
 
+
+class RecordSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.pk')
+
+    class Meta:
+        model = Record
+        fields = '__all__'
+        read_only_fields = ('owner', 'created_at')
