0b7b884b35727b5d94db045f05b5bf9bc3aa1c3e
Flynotfly <Alteria2004@gmail.com>
2025-03-26 12:19:27 +0400

    Add Meta to MetricSerializer

--- gymstat/body_metrics/serializers.py
+++ gymstat/body_metrics/serializers.py
@@ -6,3 +6,8 @@ from .models import Metric
 class MetricSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.pk")
 
+    class Meta:
+        model = Metric
+        fields = '__all__'
+        read_only_fields = ('owner', 'admin', 'created_at', 'edited_at')
+
