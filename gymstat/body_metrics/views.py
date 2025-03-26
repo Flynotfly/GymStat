a02b2ce5cb4a39062d68c23961cbd869162e5edf
Flynotfly <Alteria2004@gmail.com>
2025-03-26 12:14:27 +0400

    Create MetricListCreateAPIView and MetricRetrieveUpdateDestroyAPIView views

--- gymstat/body_metrics/views.py
+++ gymstat/body_metrics/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from django.db.models import Q
+from .models import Metric
+from .permissions import IsOwner
+from .serializers import MetricSerializer
 
-# Create your views here.
+
+class MetricListCreateAPIView(generics.ListCreateAPIView):
+    serializer_class = MetricSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        metric_type = self.request.query_params.get('type', 'all')
+
+        if metric_type == 'user':
+            return Metric.objects.filter(owner=user)
+        elif metric_type == 'admin':
+            return Metric.objects.filter(admin=True)
+        elif metric_type == 'all':
+            return Metric.objects.filter(Q(owner=user) | Q(admin=True))
+        else:
+            return Metric.objects.none()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class MetricRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = MetricSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Metric.objects.filter(Q(owner=user) | Q(admin=True))
