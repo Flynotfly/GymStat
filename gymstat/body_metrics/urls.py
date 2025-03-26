7a6f3378e7138e16271901eafabdc0c0e50f5163
Flynotfly <Alteria2004@gmail.com>
2025-03-26 14:36:57 +0400

    Add record views to urls

--- gymstat/body_metrics/urls.py
+++ gymstat/body_metrics/urls.py
@@ -7,4 +7,7 @@ app_name = "user"
 urlpatterns = [
     path("metrics/", views.MetricListCreateAPIView.as_view(), name="get-create-metrics"),
     path("metrics/<int:pk>/", views.MetricRetrieveUpdateDestroyAPIView.as_view(), name="get-edit-metric"),
+
+    path("records/", views.RecordListCreateAPIView.as_view(), name="get-create-records"),
+    path("records/<int:pk>/", views.MetricRetrieveUpdateDestroyAPIView.as_view(), name="get-edit-record"),
 ]
