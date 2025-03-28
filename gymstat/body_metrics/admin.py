40533de2b49d044f11b2438b509f61001c376051
Flynotfly <Alteria2004@gmail.com>
2025-03-28 10:39:40 +0400

    Change admin panel for Metric and Record

--- gymstat/body_metrics/admin.py
+++ gymstat/body_metrics/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 
 from .models import Record, Metric
 
-admin.register(Record)
-admin.register(Metric)
+
+@admin.register(Metric)
+class MetricAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'owner', 'unit', 'admin')
+    search_fields = ('name', 'description', 'owner__username')
+    list_filter = ('admin', 'created_at')
+    ordering = ('name',)
+
+
+@admin.register(Record)
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ('id', 'owner', 'metric', 'value', 'datetime')
+    search_fields = ('metric__name', 'owner__username')
+    list_filter = ('datetime', 'metric')
+    ordering = ('datetime',)
