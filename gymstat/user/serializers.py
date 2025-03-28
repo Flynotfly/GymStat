003b720d75d581f50f47d574a0c60d4320ea7d3e
Flynotfly <Alteria2004@gmail.com>
2025-03-28 18:38:39 +0400

    Create UserSettingsSerializer

---
+++ gymstat/user/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from .models import UserSettings
+
+
+class UserSettingsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserSettings
+        fields = '__all__'
+        read_only_fields = ['user']
