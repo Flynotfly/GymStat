9fa2e49e49fa3ae6736c94e775d0ef206105d694
Flynotfly <Alteria2004@gmail.com>
2025-03-30 16:32:54 +0400

    Create "exercises/" url

--- gymstat/training/urls.py
+++ gymstat/training/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 
-# from . import views
+from . import views
 
 app_name = "training"
 
 urlpatterns = [
-
+    path('exercises/', views.ExerciseTemplateListCreateAPIView.as_view(), name="exercises"),
+#     path('exercises/<int:pk>/', ..., name="exercise"),
 ]
