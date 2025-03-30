9ec9d7c0423184f52f34f152a509850d8070d33b
Flynotfly <Alteria2004@gmail.com>
2025-03-30 16:24:18 +0400

    Create ExerciseTemplateSerializer

---
+++ gymstat/training/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import ExerciseTemplate, validate_fields
+
+
+class ExerciseTemplateSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source="owner.pk")
+
+    class Meta:
+        model = ExerciseTemplate
+        fields = [
+            'id', 'name', 'owner', 'fields', 'description',
+            'is_admin', 'is_active', 'created_at', 'edited_at'
+        ]
+        read_only_fields = ['id', 'owner', 'is_admin', 'is_active', 'created_at', 'edited_at']
