a3a2cc10941c8530b60ca23e45e8b2e689a38a41
Flynotfly <Alteria2004@gmail.com>
2025-04-01 01:11:23 +0400

    Delete unused permission in body_metrics app

--- gymstat/body_metrics/permissions.py
+++ gymstat/body_metrics/permissions.py
@@ -12,17 +12,3 @@ class IsAdminReadOnly(permissions.BasePermission):
             return obj.admin
         else:
             return False
-
-
-class IsOwnerAllIsAdminSafe(permissions.BasePermission):
-    """
-    Custom permission to only allow owners of an object to edit or delete it.
-    """
-
-    def has_object_permission(self, request, view, obj):
-        # SAFE_METHODS (GET, HEAD, OPTIONS)
-        if request.method in permissions.SAFE_METHODS:
-            return obj.admin or obj.owner == request.user
-
-        # For unsafe methods (PUT, PATCH, DELETE)
-        return obj.owner == request.user
