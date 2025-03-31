1e1a6253bd580aad0be85716c4a57e626a24ec2c
Flynotfly <Alteria2004@gmail.com>
2025-04-01 01:12:10 +0400

    Add IsOwner and IsAdminReadOnly permissions to training app

--- gymstat/training/permissions.py
+++ gymstat/training/permissions.py
@@ -1,15 +1,14 @@
 from rest_framework import permissions
 
 
-class IsOwnerAllIsAdminSafe(permissions.BasePermission):
-    """
-    Custom permission to only allow owners of an object to edit or delete it.
-    """
-
+class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        # SAFE_METHODS
-        if request.method in permissions.SAFE_METHODS:
-            return obj.is_admin or obj.owner == request.user
-
-        # For unsafe methods
         return obj.owner == request.user
+
+
+class IsAdminReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return obj.is_admin
+        else:
+            return False
