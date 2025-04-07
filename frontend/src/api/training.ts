9468f30fae30d4fd83e48715c096f347f7c44654
Flynotfly <Alteria2004@gmail.com>
2025-04-07 19:52:53 +0400

    [Frontend] Start adding training Training request api

--- frontend/src/api/training.ts
+++ frontend/src/api/training.ts
@@ -95,6 +95,11 @@ export function deleteTrainingTemplate(
   )
 }
 
+///////////////
+// Trainings //
+///////////////
+
+
 // export function getAllTrainings(): Promise<any> {
 //   return request('GET', URL + 'trainings/');
 // }
