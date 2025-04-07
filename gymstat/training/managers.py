bf4a0a9b7ce37403b5e480944fbd3f4bc223457b
Flynotfly <Alteria2004@gmail.com>
2025-04-07 20:57:16 +0400

    [Django] Update Training Manager

--- gymstat/training/managers.py
+++ gymstat/training/managers.py
@@ -20,8 +20,6 @@ class TrainingManager(models.Manager):
         notes=None,
         exercises_data=None,
     ):
-        Exercise = apps.get_model("training", "Exercise")
-
         training = self.create(
             owner=owner,
             conducted=conducted,
@@ -29,55 +27,8 @@ class TrainingManager(models.Manager):
             title=title,
             notes=notes,
         )
-
         if exercises_data:
-            unauthorized_templates = []
-            for idx, exercise_data in enumerate(exercises_data, start=1):
-                # Expect the template to be an instance
-                exercise_template = exercise_data.get("template")
-                if not exercise_template:
-                    raise ValidationError(
-                        f"Exercise #{idx} is missing a template."
-                    )
-
-                # Check allowed condition: template must be active and either owned by the user or an admin template.
-                if not (
-                    exercise_template.is_active
-                    and (
-                        exercise_template.owner == owner
-                        or exercise_template.is_admin
-                    )
-                ):
-                    unauthorized_templates.append(exercise_template)
-
-            if unauthorized_templates:
-                raise PermissionDenied(
-                    f"Unauthorized templates: {unauthorized_templates}"
-                )
-
-            exercises_to_create = []
-            for idx, exercise_data in enumerate(exercises_data, start=1):
-                template = exercise_data.get("template")
-                order = exercise_data.get("order")
-                data = exercise_data.get("data")
-
-                try:
-                    validate_exercise_data(data, template)
-                except ValidationError as ve:
-                    raise ValidationError(
-                        f"Validation error in exercise #{idx} with template '{exercise_template.name}': {ve}"
-                    )
-
-                exercises_to_create.append(
-                    Exercise(
-                        training=training,
-                        template=exercise_template,
-                        order=order,
-                        data=data,
-                    )
-                )
-            Exercise.objects.bulk_create(exercises_to_create)
-
+            self._process_exercise_data(owner, training, exercises_data)
         return training
 
     @transaction.atomic
@@ -85,74 +36,72 @@ class TrainingManager(models.Manager):
             self,
             training,
             owner,
-            conducted=None,
+            conducted,
             template=None,
             title=None,
             notes=None,
             exercises_data=None,
     ):
-        # Ensure that only the owner can update the training.
         if training.owner != owner:
             raise PermissionDenied("You are not the owner of this training.")
 
-        # Update training fields if new values are provided.
-        if conducted is not None:
-            training.conducted = conducted
-        if template is not None:
-            training.template = template
-        if title is not None:
-            training.title = title
-        if notes is not None:
-            training.notes = notes
+        training.conducted = conducted
+        training.template = template
+        training.title = title
+        training.notes = notes
         training.save()
 
         training.exercises.all().delete()
-
         if exercises_data:
-            Exercise = apps.get_model("training", "Exercise")
-            unauthorized_templates = []
-            # Validate each exercise's template and data.
-            for idx, exercise_data in enumerate(exercises_data, start=1):
-                exercise_template = exercise_data.get("template")
-                if not exercise_template:
-                    raise ValidationError(
-                        f"Exercise #{idx} is missing a template."
-                    )
-                if not (
-                        exercise_template.is_active
-                        and (
-                                exercise_template.owner == owner
-                                or exercise_template.is_admin
-                        )
-                ):
-                    unauthorized_templates.append(exercise_template)
-            if unauthorized_templates:
-                raise PermissionDenied(
-                    f"Unauthorized templates: {unauthorized_templates}"
-                )
-
-            # Remove all existing exercises associated with this training.
-            training.exercises.all().delete()
-
-            exercises_to_create = []
-            for idx, exercise_data in enumerate(exercises_data, start=1):
-                exercise_template = exercise_data.get("template")
-                order = exercise_data.get("order")
-                data = exercise_data.get("data")
-                try:
-                    validate_exercise_data(data, exercise_template)
-                except ValidationError as ve:
-                    raise ValidationError(
-                        f"Validation error in exercise #{idx} with template '{exercise_template.name}': {ve}"
-                    )
-                exercises_to_create.append(
-                    Exercise(
-                        training=training,
-                        template=exercise_template,
-                        order=order,
-                        data=data,
-                    )
-                )
-            Exercise.objects.bulk_create(exercises_to_create)
-
+            self._process_exercise_data(owner, training, exercises_data)
         return training
+
+    @staticmethod
+    def _process_exercise_data(owner, training, exercises_data):
+        Exercise = apps.get_model("training", "Exercise")
+        unauthorized_templates = []
+        for idx, exercise_data in enumerate(exercises_data, start=1):
+            # Expect the template to be an instance
+            exercise_template = exercise_data.get("template")
+            if not exercise_template:
+                raise ValidationError(
+                    f"Exercise #{idx} is missing a template."
+                )
+
+            # Check allowed condition: template must be active and either owned by the user or an admin template.
+            if not (
+                    exercise_template.is_active
+                    and (
+                            exercise_template.owner == owner
+                            or exercise_template.is_admin
+                    )
+            ):
+                unauthorized_templates.append(exercise_template)
+
+        if unauthorized_templates:
+            raise PermissionDenied(
+                f"Unauthorized templates: {unauthorized_templates}"
+            )
+
+        exercises_to_create = []
+        for idx, exercise_data in enumerate(exercises_data, start=1):
+            template = exercise_data.get("template")
+            order = exercise_data.get("order")
+            data = exercise_data.get("data")
+
+            try:
+                validate_exercise_data(data, template)
+            except ValidationError as ve:
+                raise ValidationError(
+                    f"Validation error in exercise #{idx} with template '{template.name}': {ve}"
+                )
+
+            exercises_to_create.append(
+                Exercise(
+                    training=training,
+                    template=template,
+                    order=order,
+                    data=data,
+                )
+            )
+        Exercise.objects.bulk_create(exercises_to_create)
