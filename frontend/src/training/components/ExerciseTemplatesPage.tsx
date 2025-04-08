a86c1342a8932dd813592ded223b9152582911a1
Flynotfly <Alteria2004@gmail.com>
2025-04-08 16:57:56 +0400

    [Frontend] Create ExerciseTemplatePage component

---
+++ frontend/src/training/components/ExerciseTemplatesPage.tsx
@@ -0,0 +1,11 @@
+import {Box, Typography} from "@mui/material";
+
+export default function ExerciseTemplatesPage() {
+  return (
+    <Box sx={{ p: 2 }}>
+      <Typography component="h2" variant="h6" sx={{ mb: 2 }}>
+        Exercises
+      </Typography>
+    </Box>
+  )
+}
