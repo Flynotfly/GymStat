485a94b321edd74e85f8bbe20bbac5265b2aabc5
Flynotfly <Alteria2004@gmail.com>
2025-04-12 19:47:00 +0400

    [Frontend] Move TrainingTemplate interfaces to new file

--- frontend/src/training/types/training.d.ts
+++ frontend/src/training/types/training.d.ts
@@ -1,13 +1,3 @@
-import {NewExerciseTemplate} from "./exerciseTemplate";
-
-export interface NewTrainingTemplate{
-
-}
-
-export interface TrainingTemplate extends NewExerciseTemplate{
-  id: number,
-}
-
 export interface NewTraining{
 
 }
---
+++ frontend/src/training/types/trainingTemplate.d.ts
@@ -0,0 +1,7 @@
+export interface NewTrainingTemplate{
+
+}
+
+export interface TrainingTemplate extends NewTrainingTemplate{
+  id: number,
+}
