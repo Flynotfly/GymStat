2ae7ff1ee3b31991cc10a12fe2e145b748f59510
Flynotfly <Alteria2004@gmail.com>
2025-04-14 16:17:07 +0400

    [Frontend] Set only two card in a row in md display

--- frontend/src/training/components/ExerciseTemplatesPage.tsx
+++ frontend/src/training/components/ExerciseTemplatesPage.tsx
@@ -188,7 +188,7 @@ export default function ExerciseTemplatesPage() {
 
       <Grid2 container spacing={2}>
         {templates.map(template => (
-          <Grid2 size={{xs: 12, sm: 6,md: 4}} key={template.id}>
+          <Grid2 size={{xs: 12, md: 6, lg: 4}} key={template.id}>
             <ExerciseTemplateCard template={template} />
           </Grid2>
         ))}
