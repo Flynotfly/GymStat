f93b385475ae8e6b87bee4cd55865ede6c6a9e0f
Flynotfly <Alteria2004@gmail.com>
2025-05-03 23:27:05 +0400

    [Django][Tests] Change user other_user instances to dicts

--- gymstat/user/tests.py
+++ gymstat/user/tests.py
@@ -4,19 +4,19 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-user = User.objects.create_user(
-    email="test@example.com",
-    first_name="Test",
-    last_name="User",
-    password="testpass",
-)
+user_data = {
+    "email": "test@example.com",
+    "first_name": "Test",
+    "last_name": "User",
+    "password": "testpass",
+}
 
-other_user = User.objects.create_user(
-    email="other@example.com",
-    first_name="Other",
-    last_name="User",
-    password="otherpass",
-)
+other_user_data = {
+    "email": "other@example.com",
+    "first_name": "Other",
+    "last_name": "User",
+    "password": "otherpass",
+}
 
 login_data = {
     "email": "test@example.com",
