a2647b791a35992757f2293ff5268f474c06e9ba
Flynotfly <Alteria2004@gmail.com>
2025-05-03 22:11:43 +0400

    [Django] Create user instances in user tests

--- gymstat/user/tests.py
+++ gymstat/user/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+User = get_user_model()
+
+
+user = User.objects.create_user(
+    email="test@example.com",
+    first_name="Test",
+    last_name="User",
+    password="testpass",
+)
+
+other_user = User.objects.create_user(
+    email="other@example.com",
+    first_name="Other",
+    last_name="User",
+    password="otherpass",
+)
