feedf32a4229c9b7443fa546f005fa25a3fba490
Flynotfly <Alteria2004@gmail.com>
2025-05-04 18:20:18 +0400

    [Django][Tests] Move Record views tests to new file

--- gymstat/body_metrics/tests/views/record.py
+++ gymstat/body_metrics/tests/views/record.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from .models import Metric, Record
+from ...models import Metric, Record
 
 User = get_user_model()
 
