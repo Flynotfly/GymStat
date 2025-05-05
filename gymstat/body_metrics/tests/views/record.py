e5d298ae17b973d831c6fbdf9d73ce86b3999800
Flynotfly <Alteria2004@gmail.com>
2025-05-05 07:30:58 +0400

    [Django][Tests] Create new tests for Record views

--- gymstat/body_metrics/tests/views/record.py
+++ gymstat/body_metrics/tests/views/record.py
@@ -1,201 +1,186 @@
 from django.contrib.auth import get_user_model
 from django.urls import reverse
-from rest_framework import status
 from rest_framework.test import APITestCase
+
+from user.tests import user_data, other_user_data, login_data
 
 from ...models import Metric, Record
 
 User = get_user_model()
 
 
+def get_list_url(metric: Metric):
+    return reverse("metrics:get-create-records") + f"?metric={metric.pk}"
+
+
+def get_create_url():
+    return reverse("metrics:get-create-records")
+
+
+def get_details_url(record: Record):
+    return reverse("metrics:get-edit-record", kwargs={"pk": record.pk})
+
+
 class RecordAPITests(APITestCase):
     def setUp(self):
-        # Users
-        self.user = User.objects.create_user(
-            email="test@example.com",
-            first_name="Test",
-            last_name="User",
-            password="testpass",
-        )
+        self.user = User.objects.create_user(**user_data)
+        self.other_user = User.objects.create_user(**other_user_data)
 
-        self.other_user = User.objects.create_user(
-            email="other@example.com",
-            first_name="Other",
-            last_name="User",
-            password="otherpass",
-        )
-
-        # Metrics
-        self.user_metric = Metric.objects.create(
+        self.metric = Metric.objects.create(
             owner=self.user, name="Weight", unit="kg", admin=False
         )
-
         self.admin_metric = Metric.objects.create(
             owner=self.other_user, name="Height", unit="cm", admin=True
         )
-
         self.other_user_metric = Metric.objects.create(
             owner=self.other_user, name="BMI", unit="", admin=False
         )
 
-        # Records
         self.user_record = Record.objects.create(
             owner=self.user,
-            metric=self.user_metric,
-            value=75.0,
+            metric=self.metric,
+            value=50,
             datetime="2025-03-20T09:00:00Z",
+        )
+        self.user_fresh_record = Record.objects.create(
+            owner=self.user,
+            metric=self.metric,
+            value=100.0,
+            datetime="2025-03-21T09:00:00Z",
         )
 
         self.admin_metric_record = Record.objects.create(
             owner=self.user,
             metric=self.admin_metric,
-            value=180,
-            datetime="2025-03-21T10:00:00Z",
+            value=200,
+            datetime="2025-03-23T10:00:00Z",
         )
-
+        self.admin_metric_fresh_record = Record.objects.create(
+            owner=self.user,
+            metric=self.admin_metric,
+            value=400,
+            datetime="2025-03-25T10:00:00Z",
+        )
         self.other_user_record = Record.objects.create(
             owner=self.other_user,
             metric=self.other_user_metric,
             value=22.5,
             datetime="2025-03-22T11:00:00Z",
         )
-
-        self.client.login(email="test@example.com", password="testpass")
-
-    def test_get_records_for_user_metric(self):
-        url = (
-            reverse("metrics:get-create-records")
-            + f"?metric={self.user_metric.pk}"
+        self.admin_metric_other_user_record = Record.objects.create(
+            owner=self.other_user,
+            metric=self.admin_metric,
+            value=40,
+            datetime="2025-03-19T11:00:00Z",
         )
-        response = self.client.get(url)
+        self.new_record_data = {
+            "value": 10,
+            "datetime": "2025-04-10T11:00:00Z"
+        }
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data["count"], 1)
-        self.assertEqual(response.data["results"][0]["value"], 75.0)
+        self.client.login(**login_data)
 
-    def test_get_records_for_admin_metric(self):
-        url = (
-            reverse("metrics:get-create-records")
-            + f"?metric={self.admin_metric.pk}"
-        )
-        response = self.client.get(url)
+    def test_get_records_user_metric(self):
+        responce = self.client.get(get_list_url(self.metric))
+        self.assertEqual(responce.status_code, 200)
+        self.assertEqual(responce.data["count"], 2)
+        self.assertEqual(responce.data["results"][0]["id"], self.user_fresh_record.pk)
+        self.assertEqual(responce.data["results"][0]["value"], self.user_fresh_record.value)
+        self.assertEqual(responce.data["results"][1]["id"], self.user_record.pk)
+        self.assertEqual(responce.data["results"][1]["value"], self.user_record.value)
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data["count"], 1)
-        self.assertEqual(response.data["results"][0]["value"], 180)
+    def test_get_records_admin_metric(self):
+        responce = self.client.get(get_list_url(self.admin_metric))
+        self.assertEqual(responce.status_code, 200)
+        self.assertEqual(responce.data["count"], 2)
+        self.assertEqual(responce.data["results"][0]["id"], self.admin_metric_fresh_record.pk)
+        self.assertEqual(responce.data["results"][0]["value"], self.admin_metric_fresh_record.value)
+        self.assertEqual(responce.data["results"][1]["id"], self.admin_metric_record.pk)
+        self.assertEqual(responce.data["results"][1]["value"], self.admin_metric_record.value)
 
-    def test_get_records_metric_not_accessible(self):
-        url = (
-            reverse("metrics:get-create-records")
-            + f"?metric={self.other_user_metric.pk}"
-        )
-        response = self.client.get(url)
+    def test_get_records_other_user_metric(self):
+        responce = self.client.get(get_list_url(self.other_user_metric))
+        self.assertEqual(responce.status_code, 403)
 
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    def test_get_single_record_user_metric(self):
+        responce = self.client.get(get_details_url(self.user_record))
+        self.assertEqual(responce.status_code, 200)
+        self.assertEqual(responce.data["id"], self.user_record.pk)
+        self.assertEqual(responce.data["value"], self.user_record.value)
+
+    def test_get_single_record_admin_metric(self):
+        responce = self.client.get(get_details_url(self.admin_metric_record))
+        self.assertEqual(responce.status_code, 200)
+        self.assertEqual(responce.data["id"], self.admin_metric_record.pk)
+        self.assertEqual(responce.data["value"], self.admin_metric_record.value)
+
+    def test_get_single_record_other_user_metric(self):
+        responce = self.client.get(get_details_url(self.other_user_record))
+        self.assertEqual(responce.status_code, 403)
+
+    def test_get_single_record_admin_metric_non_owned(self):
+        responce = self.client.get(get_details_url(self.admin_metric_other_user_record))
+        self.assertEqual(responce.status_code, 403)
 
     def test_create_record_user_metric(self):
-        url = reverse("metrics:get-create-records")
-        data = {
-            "metric": self.user_metric.pk,
-            "value": 76.5,
-            "datetime": "2025-03-23T08:00:00Z",
-        }
-        response = self.client.post(url, data)
-
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Record.objects.filter(owner=self.user).count(), 3)
+        self.new_record_data["metric"] = self.metric.pk
+        responce = self.client.post(get_create_url(), self.new_record_data)
+        self.assertEqual(responce.status_code, 201)
+        records = Record.objects.filter(metric=self.metric, owner=self.user)
+        self.assertEqual(records.count(), 3)
+        self.assertEqual(records[0].value, self.new_record_data["value"])
 
     def test_create_record_admin_metric(self):
-        url = reverse("metrics:get-create-records")
-        data = {
-            "metric": self.admin_metric.pk,
-            "value": 181,
-            "datetime": "2025-03-24T08:00:00Z",
-        }
-        response = self.client.post(url, data)
+        self.new_record_data["metric"] = self.admin_metric.pk
+        responce = self.client.post(get_create_url(), self.new_record_data)
+        self.assertEqual(responce.status_code, 201)
+        records = Record.objects.filter(metric=self.admin_metric, owner=self.user)
+        self.assertEqual(records.count(), 3)
+        self.assertEqual(records[0].value, self.new_record_data["value"])
 
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(
-            Record.objects.filter(
-                owner=self.user, metric=self.admin_metric
-            ).count(),
-            2,
-        )
+    def test_create_record_other_user_metric(self):
+        self.new_record_data["metric"] = self.other_user_metric.pk
+        responce = self.client.post(get_create_url(), self.new_record_data)
+        self.assertEqual(responce.status_code, 403)
 
-    def test_create_record_not_accessible_metric(self):
-        url = reverse("metrics:get-create-records")
-        data = {
-            "metric": self.other_user_metric.pk,
-            "value": 23,
-            "datetime": "2025-03-25T08:00:00Z",
-        }
-        response = self.client.post(url, data)
-
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_get_single_owned_record(self):
-        url = reverse(
-            "metrics:get-edit-record", kwargs={"pk": self.user_record.pk}
-        )
-        response = self.client.get(url)
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data["value"], 75.0)
-
-    def test_get_single_not_owned_record(self):
-        url = reverse(
-            "metrics:get-edit-record", kwargs={"pk": self.other_user_record.pk}
-        )
-        response = self.client.get(url)
-
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_edit_owned_record(self):
-        url = reverse(
-            "metrics:get-edit-record", kwargs={"pk": self.user_record.pk}
-        )
-        data = {
-            "metric": self.user_metric.pk,
-            "value": 77,
-            "datetime": "2025-03-26T09:00:00Z",
-        }
-        response = self.client.put(url, data)
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_edit_record_user_metric(self):
+        self.new_record_data["metric"] = self.user_record.metric.pk
+        responce = self.client.put(get_details_url(self.user_record), self.new_record_data)
+        self.assertEqual(responce.status_code, 200)
         self.user_record.refresh_from_db()
-        self.assertEqual(self.user_record.value, 77)
+        self.assertEqual(self.user_record.value, self.new_record_data["value"])
 
-    def test_edit_not_owned_record(self):
-        url = reverse(
-            "metrics:get-edit-record", kwargs={"pk": self.other_user_record.pk}
-        )
-        data = {
-            "metric": self.other_user_metric.pk,
-            "value": 24,
-            "datetime": "2025-03-27T09:00:00Z",
-        }
-        response = self.client.put(url, data)
+    def test_edit_record_admin_metric(self):
+        self.new_record_data["metric"] = self.admin_metric_record.metric.pk
+        responce = self.client.put(get_details_url(self.admin_metric_record), self.new_record_data)
+        self.assertEqual(responce.status_code, 200)
+        self.admin_metric_record.refresh_from_db()
+        self.assertEqual(self.admin_metric_record.value, self.new_record_data["value"])
 
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    def test_edit_record_other_user_metric(self):
+        self.new_record_data["metric"] = self.other_user_record.metric.pk
+        responce = self.client.put(get_details_url(self.other_user_record), self.new_record_data)
+        self.assertEqual(responce.status_code, 403)
 
-    def test_delete_owned_record(self):
-        url = reverse(
-            "metrics:get-edit-record", kwargs={"pk": self.user_record.pk}
-        )
-        response = self.client.delete(url)
+    def test_edit_record_admin_metric_non_owned(self):
+        self.new_record_data["metric"] = self.admin_metric_other_user_record.metric.pk
+        responce = self.client.put(get_details_url(self.admin_metric_other_user_record), self.new_record_data)
+        self.assertEqual(responce.status_code, 403)
 
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertFalse(
-            Record.objects.filter(pk=self.user_record.pk).exists()
-        )
+    def test_delete_record_user_metric(self):
+        responce = self.client.delete(get_details_url(self.user_record))
+        self.assertEqual(responce.status_code, 204)
+        self.assertFalse(Record.objects.filter(pk=self.user_record.pk).exists())
 
-    def test_delete_not_owned_record(self):
-        url = reverse(
-            "metrics:get-edit-record", kwargs={"pk": self.other_user_record.pk}
-        )
-        response = self.client.delete(url)
+    def test_delete_record_admin_metric(self):
+        responce = self.client.delete(get_details_url(self.admin_metric_record))
+        self.assertEqual(responce.status_code, 204)
+        self.assertFalse(Record.objects.filter(pk=self.admin_metric_record.pk).exists())
 
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-        self.assertTrue(
-            Record.objects.filter(pk=self.other_user_record.pk).exists()
-        )
+    def test_delete_record_other_user_metric(self):
+        responce = self.client.delete(get_details_url(self.other_user_record))
+        self.assertEqual(responce.status_code, 403)
+
+    def test_delete_record_admin_metric_non_owned(self):
+        responce = self.client.delete(get_details_url(self.admin_metric_other_user_record))
+        self.assertEqual(responce.status_code, 403)
