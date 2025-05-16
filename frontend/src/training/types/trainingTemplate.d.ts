3c7fec059f141b92ad7d2c2e69ea7771fb8488d4
Flynotfly <Alteria2004@gmail.com>
2025-05-16 10:37:15 +0400

    [Frontend] Add types for TrainingTemplate

--- frontend/src/training/types/trainingTemplate.d.ts
+++ frontend/src/training/types/trainingTemplate.d.ts
@@ -1,5 +1,37 @@
-export interface NewTrainingTemplate{
+export type NoteField =
+  | "Text"
+  | "Datetime"
+  | "Duration"
+  | "Number"
+  | "5stars"
+  | "10stars";
 
+export interface Note {
+  Name: string;
+  Field: NoteField;
+  Required: boolean;
+  Default?: string;
+}
+
+export type SetData = {
+  [key: string]: string | number;
+};
+
+export interface Exercise {
+  Template: number;
+  Unit?: Record<string, string>;
+  Sets?: SetData[];
+}
+
+export interface TrainingTemplateData {
+  Notes?: Note[];
+  Exercises?: Exercise[];
+}
+
+export interface NewTrainingTemplate{
+  name: string,
+  description: string,
+  data: TrainingTemplateData,
 }
 
 export interface TrainingTemplate extends NewTrainingTemplate{
