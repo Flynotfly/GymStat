d1f51c9cae46424d97aa206f0d15016854a66dde
Flynotfly <Alteria2004@gmail.com>
2025-05-30 10:33:31 +0400

    [Django] Add exception messages in check_exercise_field and check_note_field

--- gymstat/training/utils.py
+++ gymstat/training/utils.py
@@ -12,14 +12,14 @@ def check_note_field(field: str, value: str) -> Literal[True]:
             try:
                 float(value)
             except ValueError:
-                raise ValidationError("")
+                raise ValidationError(f"Value '{value}' do not match type '{field}'")
             else:
                 return True
         case "Datetime":
             try:
                 datetime.strptime(value, "%Y-%m-%d %H:%M:%S")
             except ValueError:
-                raise ValidationError("")
+                raise ValidationError(f"Value '{value}' do not match type '{field}'")
             else:
                 return True
         case "Duration":
@@ -30,29 +30,29 @@ def check_note_field(field: str, value: str) -> Literal[True]:
                     continue
                 else:
                     return True
-            raise ValidationError("")
+            raise ValidationError(f"Value '{value}' do not match type '{field}'")
         case "5stars":
             try:
                 value = int(value)
             except ValueError:
-                raise ValidationError("")
+                raise ValidationError(f"Value '{value}' do not match type '{field}'")
             else:
                 if 1 <= value <= 5:
                     return True
                 else:
-                    raise ValidationError("")
+                    raise ValidationError(f"Value '{value}' do not match type '{field}'")
         case "10stars":
             try:
                 value = int(value)
             except ValueError:
-                raise ValidationError("")
+                raise ValidationError(f"Value '{value}' do not match type '{field}'")
             else:
                 if 1 <= value <= 10:
                     return True
                 else:
-                    raise ValidationError("")
+                    raise ValidationError(f"Value '{value}' do not match type '{field}'")
         case _:
-            raise ValidationError("")
+            raise ValidationError(f"Got unexpected field type '{field}'")
 
 
 def check_exercise_field(field: str, value: str) -> Literal[True]:
@@ -63,14 +63,14 @@ def check_exercise_field(field: str, value: str) -> Literal[True]:
             try:
                 int(value)
             except ValueError:
-                raise ValidationError("")
+                raise ValidationError(f"Value '{value}' do not match type '{field}'")
             else:
                 return True
         case "float":
             try:
                 float(value)
             except ValueError:
-                raise ValidationError("")
+                raise ValidationError(f"Value '{value}' do not match type '{field}'")
             else:
                 return True
         case "duration":
@@ -81,6 +81,6 @@ def check_exercise_field(field: str, value: str) -> Literal[True]:
                     continue
                 else:
                     return True
-            raise ValidationError("")
+            raise ValidationError(f"Value '{value}' do not match type '{field}'")
         case _:
-            raise ValidationError("")
+            raise ValidationError(f"Got unexpected field type '{field}'")
