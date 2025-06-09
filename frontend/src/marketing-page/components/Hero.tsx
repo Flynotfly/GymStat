75dbf0d08ab9125109060e8c625e0fe659667c49
Flynotfly <Alteria2004@gmail.com>
2025-06-09 10:14:46 +0400

    [Frontend] Typo in promo image name

--- frontend/src/marketing-page/components/Hero.tsx
+++ frontend/src/marketing-page/components/Hero.tsx
@@ -20,7 +20,7 @@ const StyledBox = styled('div')(({ theme }) => ({
   border: '1px solid',
   borderColor: (theme.vars || theme).palette.grey[200],
   boxShadow: '0 0 12px 8px hsla(220, 25%, 80%, 0.2)',
-  backgroundImage: `url(/promoExercisesLight.jpg)`,
+  backgroundImage: `url(/promoExerciseLight.jpg)`,
   backgroundSize: 'cover',
   [theme.breakpoints.up('sm')]: {
     marginTop: theme.spacing(10),
